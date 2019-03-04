c03ac3cbfac735c44bec5e2f0763e8fa282e817a
DarknessKiller <kaelvinchen3@gmail.com>
2019-03-04 15:08:27 +0800

    Restore Back To Lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo havoc_X00T-user
-add_lunch_combo havoc_X00T-userdebug
-add_lunch_combo havoc_X00T-eng
+add_lunch_combo lineage_X00T-user
+add_lunch_combo lineage_X00T-userdebug
+add_lunch_combo lineage_X00T-eng
